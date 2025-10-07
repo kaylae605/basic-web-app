aa0207408a3a7971b1142884d1ee6e2d34d9b40a
Kayla Edwards <kaylae@andrew.cmu.edu>
2025-10-07 10:00:57 -0400

    another attempt

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,7 +11,7 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
-  if (query.toLowerCase().includes("name")) {
+  if (query.toLowerCase().includes("what is your name?")) {
     return "kaylae";
   }
 
